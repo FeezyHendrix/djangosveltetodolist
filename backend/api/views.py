147ddbf12dd22f45788492f3723658cdc527a6f0
Abdulhafeez <switchdevelop1@gmail.com>
2020-01-14 04:17:22 -0800

    (feat): added viewsets class and configured urls

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,3 +1,9 @@
 from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from .serializers import TodoListSerializer
+from .models import TodoList
+
+class TodoListViewSet(viewsets.ModelViewSet):
+    queryset = TodoList.objects.all()
+    serializer_class = TodoListSerializer
