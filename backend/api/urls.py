df3dad2957c70bf862f7034b8976a647429f6cf5
Abdulhafeez <switchdevelop1@gmail.com>
2020-01-14 04:28:55 -0800

    (feat): added url routers

--- backend/api/urls.py
+++ backend/api/urls.py
@@ -3,8 +3,8 @@ from rest_framework import routers
 from . import views
 
 router = routers.DefaultRouter()
-router.register(r'api', views.TodoListViewSet)
-
+router.register(r'todolist', views.TodoListViewSet)
+router.register(r'todo', views.TodoViewSet)
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
 urlpatterns = [
