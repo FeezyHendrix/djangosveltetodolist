d82e3dcaa01bbbbc6a195cb178ad1e8b57a68ca8
Abdulhafeez <switchdevelop1@gmail.com>
2020-01-14 04:27:14 -0800

    (feat) todo viewsets

--- backend/api/views.py
+++ backend/api/views.py
@@ -1,9 +1,14 @@
 from django.shortcuts import render
 from rest_framework import viewsets
 
-from .serializers import TodoListSerializer
+from .serializers import TodoListSerializer, TodoSerializer
 from .models import TodoList
 
 class TodoListViewSet(viewsets.ModelViewSet):
     queryset = TodoList.objects.all()
     serializer_class = TodoListSerializer
+
+
+class TodoViewSet(viewsets.ModelViewSet):
+    queryset = TodoList.objects.all()
+    serializer_class = TodoSerializer
